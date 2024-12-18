26f1597fcbff1927299f844f916d78ec1a5a55a5
manukumar232004 <manukumar23.200.4@gmail.com>
2024-12-18 20:28:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -252,11 +252,11 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 # Shortlink Info
 
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
 
-SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'omegalinks.in')
 
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
+SHORTLINK_API = environ.get('SHORTLINK_API', '877f10a400c5fb4c7c5062e00db772cdc7cb6429')
 
 TUTORIAL = environ.get('TUTORIAL', '') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
